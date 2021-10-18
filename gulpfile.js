4fd8a39a71521df3e2a24be08e7ed54a1364b03c
TonnyHawk <anton.veremko@gmail.com>
2021-10-18 20:57:51 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { watch } = require('fs');
+let sass = require('gulp-sass'),
+{dest, src, series, watch} = require('gulp'),
+gulp = require('gulp'),
+sourcemaps = require('gulp-sourcemaps');
+
+
+function css(){
+   return src('src/styles/main.scss')
+   .pipe(sourcemaps.init())
+   .pipe(sass())
+   .pipe()
+   .pipe(sourcemaps.write())
+   .pipe(dest('dist/css/'));
+}
+
+function watchCss(){
+   watch('src/styles/**/*.scss', css)
+}
+
+exports.default = series(css, watchCss)
