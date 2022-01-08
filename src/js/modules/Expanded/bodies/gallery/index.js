66e751c39b9392ba89409bdd9bef5595dd45eaa7
TonnyHawk <anton.veremko@gmail.com>
2022-01-08 17:58:17 +0200

    showcase fix

--- src/js/modules/Expanded/bodies/gallery/index.js
+++ src/js/modules/Expanded/bodies/gallery/index.js
@@ -104,14 +104,14 @@ export default function GalleryBody(properties){
                                  value={showcase}
                                  onChange={(e)=>funcs.handleChange(e)}>
                                     <option value={'null'}>Не визначено</option>
-                                    <option value={0}>0</option>
-                                    <option value={1}>1</option>
-                                    <option value={2}>2</option>
-                                    <option value={3}>3</option>
-                                    <option value={4}>4</option>
-                                    <option value={5}>5</option>
-                                    <option value={6}>6</option>
-                                    <option value={7}>7</option>
+                                    <option value={'0'}>0</option>
+                                    <option value={'1'}>1</option>
+                                    <option value={'2'}>2</option>
+                                    <option value={'3'}>3</option>
+                                    <option value={'4'}>4</option>
+                                    <option value={'5'}>5</option>
+                                    <option value={'6'}>6</option>
+                                    <option value={'7'}>7</option>
                                  </select>
                            </div>
                         </form>
