54ff5d574d302a40482fcb2ea936edbf7c057dea
TonnyHawk <anton.veremko@gmail.com>
2022-01-10 17:57:10 +0200

    favicon and title added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ let path  = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   mode: 'development',
+   mode: 'production',
    entry: {
       app: ['@babel/polyfill', './src/js/index.js']
    },
