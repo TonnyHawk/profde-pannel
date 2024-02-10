91769e6cb84c2820b91a3d44f07641b730f30f6a
Anthonii Veremko <anton.veremko@gmail.com>
2024-02-10 20:38:40 +0100

    Vite build up and running

--- vite-project/src/js/globals.js
+++ vite-project/src/js/globals.js
@@ -1,3 +1,3 @@
-let serverUrl = 'http://91.219.61.167:3000/'
-// let serverUrl = 'http://127.0.0.1:3000/';
+// let serverUrl = 'http://91.219.61.167:3000/'
+let serverUrl = 'http://127.0.0.1:5002/';
 export default serverUrl;
